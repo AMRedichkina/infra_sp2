4345cb17fc8aeba744411ffa75b8520a87f98fcb
Ваше Имя <Alexandra@MacBook-Air.local>
2022-11-04 14:06:13 +0100

    V.2

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -10,7 +10,7 @@ load_dotenv()
 SECRET_KEY = os.environ.get("SECRET_KEY", default='1234')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
